7a91ad982d0005f81c4adaa986501f3983359647
jonjon <neumerance@live.com>
2018-03-14 08:53:44 +0800

    saving progress

--- server/server.js
+++ server/server.js
@@ -95,36 +95,36 @@ apiRoutes.post('/authenticate', function(req, res) {
 }); 
 
 // route middleware to verify a token
-// apiRoutes.use(function(req, res, next) {
+apiRoutes.use(function(req, res, next) {
 
-//   // check header or url parameters or post parameters for token
-//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-//   // decode token
-//   if (token) {
+  // decode token
+  if (token) {
 
-//     // verifies secret and checks exp
-//     jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-//       if (err) {
-//         return res.json({ success: false, message: 'Failed to authenticate token.' });    
-//       } else {
-//         // if everything is good, save to request for use in other routes
-//         req.decoded = decoded;    
-//         next();
-//       }
-//     });
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
 
-//   } else {
+  } else {
 
-//     // if there is no token
-//     // return an error
-//     return res.status(403).send({ 
-//         success: false, 
-//         message: 'No token provided.' 
-//     });
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
 
-//   }
-// });  
+  }
+});  
 
 // route to return all users (GET http://localhost:4000/api/users)
 apiRoutes.get('/users', function(req, res) {
