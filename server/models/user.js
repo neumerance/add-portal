febebbfe723d9ab52d97d051a62e996ed069b607
jonjon <neumerance@live.com>
2018-03-15 08:10:59 +0800

    UPDATE

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-module.exports = mongoose.model('User', new Schema({ 
-    email: String, 
-    password: String, 
-    role: Number // 1 admin, 1 addpro, 1 viewer 
-}));
+const UserSchema = new Schema({ 
+  email: { 
+    type: String, 
+    require: [true, 'Email is required.'],
+    unique: true
+  }, 
+  password: String, 
+  role: { type: Number, require: [true, 'Role is not defined'] },  // 1 admin, 1 addpro, 1 viewer
+  created: { type: Date, default: Date.now } 
+});
+module.exports = mongoose.model('User', UserSchema);
