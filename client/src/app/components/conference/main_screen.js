3e9480d90e7a5c666aabf8953b8f657cef3dee9e
jonjon <neumerance@live.com>
2018-05-05 23:05:35 +0800

    conference is now working

--- client/src/app/components/conference/main_screen.js
+++ client/src/app/components/conference/main_screen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './main_screen.scss';
 import auth from '../../services/auth/index';
+import ConferenceParticipants from './participants';
 const user = auth.getUserInfo();
 
 export default class ConferenceMainScreen extends React.Component {
@@ -8,7 +9,7 @@ export default class ConferenceMainScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      participants: []
+      streamEvents: []
     }
     this.localStream = Erizo.Stream({audio: true, video: true, data: false, attributes: {name: user.email}});
     this.room = Erizo.Room({token: this.props.roomToken});
@@ -20,78 +21,58 @@ export default class ConferenceMainScreen extends React.Component {
   }
 
   init() {
-    this.localStream.addEventListener("access-accepted", () => {
-      this.whenConnected();
-      this.whenSubscribedToStreams();
-      this.whenAddedAStream();
-      this.localStream.play("main-screen");
+    const self = this;
+    self.localStream.addEventListener("access-accepted", () => {
+      self.localStream.play("main-screen");
       setTimeout(() => {
-        this.room.connect();
+        self.room.addEventListener("room-connected", function (roomEvent) {
+          self.room.publish(self.localStream);
+          self.subscribeToStreams(roomEvent.streams);
+        });
+
+        self.room.addEventListener("stream-subscribed", (streamEvent) => {
+          const streamEvents = this.state.streamEvents;
+          streamEvents.push(streamEvent);
+          self.setState({ streamEvents });
+        });
+
+        self.room.addEventListener("stream-added", (streamEvent) => {
+          self.room.subscribe(streamEvent.stream);
+        });
+
+        self.room.connect();
       }, 1000);
     });
   }
 
-  whenConnected() {
-    this.room.addEventListener("room-connected", (roomEvent) => {
-      console.log('AddClient: ', 'You are now connected to the room.');
-      console.log('AddClient: ', this.localStream);
-      this.room.publish(this.localStream);
-      // this.subscribeToStreams(roomEvent.streams);
-    });
-  }
-
-  subscribeToStreams(streams = []) {
-    streams.forEach(stream => {
-      if (stream.getID() !== this.localStream.getID()) {
-        this.room.subscribe(stream);
+  subscribeToStreams(streams) {
+    for (var index in streams) {
+      var stream = streams[index];
+      if (this.localStream.getID() !== stream.getID()) {
+          this.room.subscribe(stream);
       }
-    });
-  };
-
-  whenSubscribedToStreams() {
-    this.room.addEventListener("stream-subscribed", (streamEvent) => {
-      const participants = this.state.participants;
-      participants.push(streamEvent);
-      this.setState({ participants }, () => {
-        this.state.participants.forEach((participant, key) => {
-          const stream = participant.stream;
-          stream.play(`participant-${stream}`);
-        });
-      });
-    });
+    }
   }
-
-  whenAddedAStream() {
-    this.room.addEventListener("stream-added", (streamEvent) => {
-      console.log('AddClient: ', 'Stream added');
-      // const streams = [];
-      // streams.push(streamEvent.stream);
-      // this.subscribeToStreams(streams);
-    });
-  }
-
-  // whenAStreamIsRemoved() {
-  //   this.room.addEventListener("stream-removed", (streamEvent) => {
-  //     const stream = streamEvent.stream;
-  //     if (stream.elementID !== undefined) {
-
-  //     }
-  //   });
-  // }
 
   renderParticipants() {
-    return this.state.participants.map((participant, key) => {
+    return this.state.streamEvents.map((streamEvent, key) => {
+      setTimeout(() => {
+        streamEvent.stream.play(`participant-${streamEvent.stream.getID()}`);
+      }, 1000);
       return(
-        <div id={`participant-${participant.stream.getID()}`} key={key} className={styles.participantsPanel.participants}></div>
+        <div id={`participant-${streamEvent.stream.getID()}`}
+             key={key}
+             className={styles.participants}>
+        </div>
       );
     });
   }
 
   render() {
     return(
-      <div id="main-screen" className={styles.mainScreen}>
-        <div className={styles.participantsPanel}>
-          {this.renderParticipants()}
+      <div>
+        <ConferenceParticipants />
+        <div id="main-screen" className={styles.mainScreen}>
         </div>
       </div>
     );
---
+++ client/src/app/components/conference/participants.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ConferenceParticipants extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+
+}
